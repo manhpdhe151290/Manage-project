57a49f1a776d90e34874b86f48382971abb0c153
ducmanh <manhpd151290@fpt.edu.vn>
2023-06-13 22:59:58 +0700

    update

--- pages/company/[id].tsx
+++ pages/company/[id].tsx
@@ -25,47 +25,167 @@ function CompanyDetail() {
   }, [id])
   console.log(data)
   return (
-    <div>
-      <div>
-        <h1 className='text-center uppercase'>{data?.companyName}</h1>
-      </div>
+    <div
+      style={{
+        width: '100%',
+        minHeight: '100vh',
+        backgroundImage: 'linear-gradient(to left bottom, #ffffff,  #333)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        padding: '7.5rem',
+      }}
+    >
       <div
         style={{
           display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          padding: '50px',
+          alignContent: 'center',
+          width: '60%',
+          height: '100%',
+          maxWidth: '90rem',
+          margin: '0 auto',
+          borderRadius: '10px',
         }}
       >
-        <h1
+        <div
           style={{
-            fontSize: '2.5rem',
-            marginTop: '20px',
-            marginBottom: 0,
+            width: '50%',
+            padding: '2rem 2rem 8rem',
+            background:
+              'linear-gradient(to top right, #ffffff,  #333) no-repeat center / cover',
+            borderRadius: '10px',
+            position: 'relative',
+            height: '80%',
+            display: 'flex',
+            flexDirection: 'column',
             textAlign: 'center',
           }}
         >
-          {data?.numberPhone}
-        </h1>
-        <p
+          <span
+            style={{
+              color: '#333',
+              fontSize: '4rem',
+              fontWeight: '500',
+              height: '20rem',
+              opacity: '0.8',
+            }}
+          >
+            Thông tin công ty
+          </span>
+          <ul
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: 'auto',
+            }}
+          >
+            <li
+              style={{
+                width: '1.5rem',
+                height: '1.5rem',
+                backgroundColor: 'white',
+                borderRadius: '2rem',
+                margin: '0 5px',
+                opacity: '0.5',
+                cursor: 'pointer',
+              }}
+            ></li>
+            <li
+              style={{
+                width: '1.5rem',
+                height: '1.5rem',
+                backgroundColor: 'white',
+                borderRadius: '2rem',
+                margin: '0 5px',
+                opacity: '0.5',
+                cursor: 'pointer',
+              }}
+            ></li>
+            <li
+              style={{
+                width: '1.5rem',
+                height: '1.5rem',
+                backgroundColor: 'white',
+                borderRadius: '2rem',
+                margin: '0 5px',
+                opacity: '0.5',
+                cursor: 'pointer',
+              }}
+            ></li>
+          </ul>
+        </div>
+        <div
           style={{
-            maxWidth: '400px',
-            height: 'auto',
-            marginTop: '20px',
-            marginBottom: '40px',
+            width: '50%',
+            backgroundColor: 'white',
+            borderRadius: '0 10px 10px 0',
+            height: '50%',
           }}
         >
-          {data?.representativeName}
-        </p>
-        <p
-          style={{
-            marginTop: '20px',
-            fontSize: '1.2rem',
-            textAlign: 'center',
-          }}
-        >
-          Website: <div>{data?.gmail}</div>
-        </p>
+          <h2
+            style={{
+              color: '#6b6b6b',
+              fontSize: '2rem',
+              fontWeight: '600',
+              marginBottom: '1rem',
+            }}
+          >
+            CompanyName: {data?.companyName}
+          </h2>
+          <p
+            style={{
+              color: '#6b6b6b',
+              fontSize: '2rem',
+              marginBottom: '2rem',
+            }}
+          >
+            Gmail: {data?.gmail}
+            <div
+              style={{
+                color: '#6b6b6b',
+                fontSize: '2rem',
+                marginBottom: '2rem',
+              }}
+            >
+              NumberPhone: {data?.numberPhone}
+            </div>
+          </p>
+          <p
+            style={{
+              fontSize: '1.2rem',
+              lineHeight: '2',
+              color: '#6b6b6b',
+              marginBottom: '3rem',
+            }}
+          >
+            RepresentativeName: {data?.representativeName}
+          </p>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              color: '#cfcfcf',
+              fontSize: '1.6rem',
+            }}
+          ></div>
+          <button
+            style={{
+              marginTop: '3rem',
+              padding: '25px',
+              backgroundColor: '#0070f3',
+              fontSize: '18px',
+              border: '0',
+              borderRadius: '1rem',
+              cursor: 'pointer',
+              display: 'inline-block',
+              boxShadow: '0 5px 10px 0 rgba(130, 201, 30, 0.5)',
+              width: '100%',
+              background: 'rgb(130, 201, 30)',
+            }}
+          >
+            View
+          </button>
+        </div>
       </div>
     </div>
   )
